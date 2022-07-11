e95b73bc4c0aa0354ece15fa5fc9b490c1280558
Rafe Ibrahim <rafei@dhcp-100-109.eduroam.aalto.fi>
2022-07-11 16:28:17 +0300

    completed notes app and audited exercises until part2 form

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  //In order to get our page to update when new notes are added 
+  //it's best to store the notes in the App component's state
+  const [newNote, setNewNote] = useState('a new note...')
+  const [notes, setNotes ] = useState(props.notes);
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log('button clicked', event.target[0].value);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  };
+  
+
+  const handleNoteChange = (event) => {
+    console.log('event', event);
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => {
-          return <Note key={note.id} note={note}/>;
+        {notesToShow.map((note) => {
+          return <Note key={note.id} note={note} />;
         })}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
