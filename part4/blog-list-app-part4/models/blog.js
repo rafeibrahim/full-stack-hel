4deb09a98a7e3bcba5dcc39023322d6c99ee4835
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2023-07-14 10:40:40 +0300

    updated schema for blog post, made title author and url required and min length  5 chars

--- part4/blog-list-app-part4/models/blog.js
+++ part4/blog-list-app-part4/models/blog.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
+  title: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  author: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  url: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   likes: Number,
 });
 
